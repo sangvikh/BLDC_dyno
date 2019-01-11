3fa489ccc6eb60bb87fa94d70744ff21c5e275a3
joeybane <43299500+joeybane@users.noreply.github.com>
2019-01-11 16:48:16 +0100

    Update BLDC_dyno.ino
    Changed to thalexanders CAN library, extended CAN working now

--- BLDC_dyno.ino
+++ BLDC_dyno.ino
@@ -9,54 +9,66 @@
 
 //Constants
 const int spiPin = 10;
-const int ledPin = 2;
+const int ledPin = 3;
+const int intPin = 2;
 
 //Initialize MCP
 MCP_CAN CAN(spiPin);
 
+//Global variables
+long unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[8];
+char msgString[128];                        // Array to store serial string
+
 void setup()
 {
-    Serial.begin(115200);
-    pinMode(ledPin,OUTPUT);
+  Serial.begin(115200);
+  
+  // Initialize MCP2515 running at 8MHz with a baudrate of 250kb/s and the masks and filters disabled.
+  if(CAN.begin(MCP_ANY, CAN_250KBPS, MCP_8MHZ) == CAN_OK)
+    Serial.println("MCP2515 Initialized Successfully!");
+  else
+    Serial.println("Error Initializing MCP2515...");
+  
+  CAN.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
 
-    while (CAN_OK != CAN.begin(CAN_250KBPS, MCP_8MHz))              // init can bus : baudrate = 250k
-    {
-        Serial.println("CAN BUS Shield init fail");
-        Serial.println("Init CAN BUS Shield again");
-        delay(100);
-    }
-    Serial.println("CAN BUS Shield init ok!");
+  pinMode(intPin, INPUT);                            // Configuring pin for /INT input
+  
+  Serial.println("MCP2515 Library Receive Example...");
 }
 
 void loop()
 {
-    unsigned char len = 0;
-    unsigned char buf[8];
-
-    //Read CAN messages
-    if(CAN_MSGAVAIL == CAN.checkReceive())            // check if data coming
-    {
-        CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
-        
-        unsigned long canId = CAN.getCanId();
-
-        Serial.println("-----------------------------");
-        Serial.print("get data from ID: 0x");
-        Serial.println(canId, HEX);
-
-        for(int i = 0; i<len; i++)    // print the data
-        {
-            Serial.print(buf[i]);
-            Serial.print("\t");
-        }
-        Serial.println();
-    }
-
-    //Send CAN message
-    char msg[8] = {1, 3, 3, 7};
-    len = 4;
-    CAN.sendMsgBuf(0x70, 1, len, msg);
+  //Read CAN messages
+  if(!digitalRead(intPin))                         // If CAN0_INT pin is low, read receive buffer
+  {
+    CAN.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
     
-    //Delay
-    delay(1000);
+    if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
+      sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
+    else
+      sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
+  
+    Serial.print(msgString);
+  
+    if((rxId & 0x40000000) == 0x40000000){    // Determine if message is a remote request frame.
+      sprintf(msgString, " REMOTE REQUEST FRAME");
+      Serial.print(msgString);
+    } else {
+      for(byte i = 0; i<len; i++){
+        sprintf(msgString, " 0x%.2X", rxBuf[i]);
+        Serial.print(msgString);
+      }
+    }
+        
+    Serial.println();
+  }
+
+  //Send CAN message
+  char msg[8] = {1, 3, 3, 7};
+  int len = 4;
+  CAN.sendMsgBuf(0x70, 1, len, msg);
+  CAN.sendMsgBuf(0x71, 1, len, msg);
+  delay(100);   // send data per 100ms
 }
