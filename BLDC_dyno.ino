0994d6b780d59336411655887e86fad8da2d90d4
joeybane <43299500+joeybane@users.noreply.github.com>
2019-01-21 16:15:27 +0100

    Changed VESC control to UART

--- BLDC_dyno.ino
+++ BLDC_dyno.ino
@@ -1,74 +1,48 @@
 /* BLDC dyno
- * Controls two VESC's via CAN bus
+ * Controls two VESC's via UART, controlled via CAN, SD card logging
  * 11" HMI
  */
 
 //Includes
-#include <SPI.h>
-#include "mcp_can.h"
+#include <VescUart.h>
+#include <FlexCAN.h>
 
-
-//Constants
-const int spiPin = 10;
-const int ledPin = 3;
-const int intPin = 2;
-
-//Initialize MCP
-MCP_CAN CAN(spiPin);
+//Initiate classes
+VescUart Brake;   //Brake VESC
+//VescUart DUT;     //Device under test VESC
+FlexCAN CAN(500000);
 
 //Global variables
-long unsigned int rxId;
-unsigned char len = 0;
-unsigned char rxBuf[8];
-char msgString[128];                        // Array to store serial string
+static CAN_message_t msg;
 
-void setup()
-{
+void setup() {
+
+  //Setup debug serial
   Serial.begin(115200);
-  
-  // Initialize MCP2515 running at 8MHz with a baudrate of 250kb/s and the masks and filters disabled.
-  if(CAN.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK)
-    Serial.println("MCP2515 Initialized Successfully!");
-  else
-    Serial.println("Error Initializing MCP2515...");
-  
-  CAN.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
-  pinMode(intPin, INPUT);                            // Configuring pin for /INT input
-  
-  Serial.println("MCP2515 Library Receive Example...");
+
+  //Setup serial to brake VESC
+  Serial1.begin(115200);
+
+  //Setup serial to DUT VESC
+  //Serial2.begin(115200);
+
+  //Define which serial ports to use
+  Brake.setSerialPort(&Serial1);
+  //DUT.setSerialPort(&Serial2);
+
+  //Begin CAN communication
+  CAN.begin();
+
+  delay(100); //Wait until communication is initiated
 }
 
 void loop()
-{
-  //Read CAN messages
-  if(!digitalRead(intPin))                         // If CAN0_INT pin is low, read receive buffer
-  {
-    CAN.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
+{  
+  //Get data from brake
+  Brake.getVescValues();
+  Serial.print("RPM: "); Serial.println(Brake.data.rpm);
+  Serial.print("A: "); Serial.println(Brake.data.avgMotorCurrent);
+  Brake.setBrakeCurrent(1.0);
     
-    if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
-      sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
-    else
-      sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
-  
-    Serial.print(msgString);
-  
-    if((rxId & 0x40000000) == 0x40000000){    // Determine if message is a remote request frame.
-      sprintf(msgString, " REMOTE REQUEST FRAME");
-      Serial.print(msgString);
-    } else {
-      for(byte i = 0; i<len; i++){
-        sprintf(msgString, " 0x%.2X", rxBuf[i]);
-        Serial.print(msgString);
-      }
-    }
-        
-    Serial.println();
-  }
-
-  //Send CAN message
-  char msg[8] = {1, 3, 3, 7};
-  int len = 4;
-  CAN.sendMsgBuf(0x70, 0, len, msg);
-  CAN.sendMsgBuf(0x71, 1, len, msg);
-  delay(100);   // send data per 100ms
+  delay(500);
 }
