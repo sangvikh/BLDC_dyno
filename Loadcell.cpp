03b909afb679f277694bc1789cb9d2e41b82101e
joeybane <43299500+joeybane@users.noreply.github.com>
2019-01-28 13:29:56 +0100

    Revert "fucked up config.h"
    This reverts commit bcaaffe848b797a79cd4710209091030a3ee01e1.

--- Loadcell.cpp
+++ Loadcell.cpp
@@ -1,9 +1,12 @@
 #include "LoadCell.h"
 #include "HX711-multi.h"
 #include "functions.h"
-#include "config.h"
 
-HX711MULTI scales(CHANNEL_COUNT, DOUTS, CLK);   //HX711-multi library
+//HX711 scale setup
+#define CLK 5      // clock pin to the load cell amp
+byte DOUTS[1] = {6};    //data pins
+#define CHANNEL_COUNT sizeof(DOUTS)/sizeof(byte)
+HX711MULTI scales(CHANNEL_COUNT, DOUTS, CLK);
 
 LoadCell::LoadCell(){}
 LoadCell::~LoadCell(){}
--- config.cpp
+++
@@ -1,9 +0,0 @@
-#include "config.h"
-
-CANbaud = 500000;
-serialBaud = 115200;
-
-//HX711 scale setup
-CLK = 5;      // clock pin to the load cell amp
-DOUTS[1] = {6};    //data pins
-CHANNEL_COUNT = sizeof(DOUTS)/sizeof(byte);
